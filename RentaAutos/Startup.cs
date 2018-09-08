762022cc1462dcc2e90af7fc23865423475ea479
Sala K 403 <salak403@itmsalas.edu.co>
2018-09-08 10:57:06 -0500

    Agregar archivos de proyecto.

---
+++ RentaAutos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentaAutos.Startup))]
+namespace RentaAutos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
